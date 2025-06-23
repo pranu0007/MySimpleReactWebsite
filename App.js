d755b1157e63ba04fbfb59f0c0bcf11701d68065
pranu0007 <pritamrj420@gmail.com>
2025-06-23 13:24:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Welcome to My Simple React Website</h1>
+      </header>
+      <main>
+        <p>This is a basic React app created from scratch!</p>
+        <p>You can build cool things with React 🚀</p>
+      </main>
+      <footer>
+        <p>&copy; 2025 MyReactSite</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
